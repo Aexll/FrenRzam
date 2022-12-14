9871608515802e781ece284b14203c772bad7307
Aexl <axellavignepro@gmail.com>
2022-12-14 17:16:13 +0100

    map selector

---
+++ DoodleStars/Assets/Core/UI/S_MapSelector.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class S_MapSelector : MonoBehaviour
+{
+
+    public UnityEvent<string> OnSelectMap;
+    public string selectedMap;
+
+    public void SelectMap(string mapName)
+    {
+        if(mapName!=null && mapName != selectedMap)
+        {
+            selectedMap = mapName;
+            PlayerPrefs.SetString("map", selectedMap);
+        }
+    }
+
+    private void Start()
+    {
+        SelectMap(PlayerPrefs.GetString("map"));
+    }
+}
